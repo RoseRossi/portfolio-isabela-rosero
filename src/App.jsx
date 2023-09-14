e699758d2fc5e14e66e0befe3f9e4512e2cfa3ae
Isabela <isabela.rosero@correounivalle.edu.co>
2023-09-13 23:10:03 -0500

    Actividad 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import Info from "./Info/Info";
 
 const App = () => {
     return (
         <>
+        <Info 
+            nombre={"Isabela Rosero"}
+            biografia={"Student"}
+         />
          <Canvas
             camera={{position: [2,0,5]}}
          >
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,17 +4,37 @@ import { useFrame } from "@react-three/fiber";
 
 const Experience = ({title, info}) => {
     const boxRef = useRef();
+    const coneRef = useRef();
+    const sphereRef = useRef();
+    const torusRef = useRef();
+
     useFrame((state, delta) => {
         boxRef.current.rotation.x += 1 * delta;
+        coneRef.current.position.y = Math.sin(state.clock.getElapsedTime());
+        sphereRef.current.rotation.y = Math.cos(state.clock.getElapsedTime());
+        torusRef.current.position.x = Math.sin(state.clock.getElapsedTime()) + 5;
     });
+    
     return (
         <>
         <OrbitControls makeDefault />
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={2} />
-        <mesh ref={boxRef}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="DarkCyan"  />
+        <mesh ref={boxRef} position={[-5.5,0,0]}>
+            <boxGeometry args={[1,1,1]} />
+            <meshStandardMaterial color="#54C1EF"  />
+        </mesh>
+        <mesh ref={coneRef} position={[-3,0,0]}>
+            <coneGeometry args={[1, 2, 8]} />
+            <meshPhongMaterial color="#54C1EF"/>
+        </mesh>
+        <mesh ref={sphereRef} position={[0,0,0]}>
+            <sphereGeometry args={[1, 32, 16]} />
+            <meshToonMaterial color="#54C1EF"/>
+        </mesh>
+        <mesh ref={torusRef} position={[3,0,0]}>
+            <torusGeometry args={[1, -0.4, 50, 500]} />
+            <meshLambertMaterial color="#54C1EF"/>
         </mesh>
         </>
     );
